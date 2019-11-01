ecd217263937b70dd845dd439c10e603d8265a49
Amy Rockie <rockie1004@gmail.com>
2019-10-31 23:22:49 -0500

    Removed songKey from Song class (if we are saving the map to the file, the key will be saved from map going to file and stay with the object that way)

--- KaraokeSongs/FileManagement.h
+++ KaraokeSongs/FileManagement.h
@@ -1,6 +1,4 @@
 #pragma once
-#ifndef USEFULTHINGS_H
-#define USEFULTHINGS_H
 
 #include <string>
 #include <iostream>
--- KaraokeSongs/Song.cpp
+++ KaraokeSongs/Song.cpp
@@ -1,9 +1,7 @@
 #include "Song.h"
 Song::Song(string newTitle, Artist newArtist) { title = newTitle; artist = newArtist; };
-void Song::setSongKey(int newKey) { songKey = newKey; };
 void Song::setTitle(string newTitle) { title = newTitle; };
 void Song::setArtist(Artist newArtist) { artist = newArtist; };
-int Song::getSongKey() { return songKey; };
 string Song::getTitle() { return title; };
 Artist Song::getArtist() { return artist; };
 string Song::display() { return title + " by " + "dummyArtist"; }
