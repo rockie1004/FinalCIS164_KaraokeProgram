72b4a872b0a573413558a299ab1dfa8c7fa59cc2
Amy Rockie <rockie1004@gmail.com>
2019-11-08 22:18:40 -0600

    Got set up for file read/write, almost there

--- KaraokeSongs/KaraokeSongs.cpp
+++ KaraokeSongs/KaraokeSongs.cpp
@@ -9,25 +9,34 @@
 #include "maps.h"
 #include <map>
 
-
+void startup();
 int main()
 {
+	startup();
+	
 	string artistKey = "dummyArtist";
 
 
 	Song s = Song("manual input", artistKey);
 	for (int i = 0; i < 5; i++) {
 		Song tempSong = userInputSong(artistKey);
-		if (addSongToCatalogs(tempSong, artistKey)) { cout << "Added to map\n"; }
+		if (addSongToCatalogs(tempSong)) { cout << "Added to map\n"; }
 		else { cout << "Error\n"; };
 	}
 
+	
+	//not ready...WriteMapToFile(songMap, songFstream);
+
+
 	cout << "For Testing, display contents of map \n";
-		displayMap(songCatalogBySong);
-		//displayMap(songCatalogByArtist);
+		displayMap(songMap);
+	//	//displayMap(songCatalogByArtist);
 
 	};
 
+void startup() {
+	cout << "open song fstream: " << openFileInOut(songFstream, songFileTXT) << endl;
+};
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
